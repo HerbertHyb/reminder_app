1bc48a3531ff5c393022e90dbde107232d1ad54e
1463127791 <1463127791@qq.com>
2025-05-17 19:51:37 +0200

    飞洒发生

--- App.vue
+++ App.vue
@@ -7,8 +7,6 @@
     onLaunch: function() {
       this.$u.http.get('/auth/info', {}, {}).then(res => {
         console.log('123')
-        console.log('124')
-        console.log('125')
         // private String username;
         //    private String phone;
         //    private String email;
